69a4c36acf0eaaea3db72aeec93312036afc0974
Alex <morrison.alexx@gmail.com>
2021-02-04 13:15:55 +0300

    edit try-catch block

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
 'use strict';
 
 // проверка длины и типа комментария
-const isCommentOverLimit = (comment, maxLength = 140) => {
-  try {
-    if (typeof comment != 'string') {
-      throw new TypeError('Not a String');
-    } else {
-      return comment.length > maxLength;
-    }
-  } catch(err) {
-    return err.name + ':' + err.message
+const isValidComment = (comment, maxLength = 140) => {
+  if (typeof comment != 'string') {
+    throw new TypeError('Not a String');
+  } else {
+    return comment.length > maxLength;
   }
 }
 
-isCommentOverLimit(15, 2);
+try {
+  isValidComment('комментарий');
+} catch (err) {
+  /* eslint-disable no-console */
+  console.error(err.name + ':' + err.message)
+}
 
 
 // Возвразщает случайное целое число из диапозона
 const getRandomIntIncl = (min, max) => {
-  try {
-    if (isNaN(min) || isNaN(max)) {
-      throw new TypeError('Not a number');
-    } else if (min < 0 || max < 0 ) {
-      throw new RangeError('Outside of valid range');
-    } else if (max < min) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      [min, max] = [max, min]
-    }
-  } catch(err) {
-    return err.name + ' : ' + err.message;
+  if (isNaN(min) || isNaN(max)) {
+    throw new TypeError('Not a number');
+  } else if (min < 0 || max < 0 ) {
+    throw new RangeError('Outside of valid range');
+  } else if (max < min) {
+    min = Math.floor(min);
+    max = Math.floor(max);
+    [min, max] = [max, min]
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomIntIncl(5, 15);
+try {
+  getRandomIntIncl(5, 15);
+} catch (err) {
+  /* eslint-disable no-console */
+  console.error(err.name + ' : ' + err.message);
+}
