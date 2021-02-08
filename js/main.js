bad02b5676364e108bec75bb3986b643a2c550e8
Alex <morrison.alexx@gmail.com>
2021-02-08 21:44:26 +0300

    Array function

--- js/main.js
+++ js/main.js
@@ -37,3 +37,61 @@ try {
   /* eslint-disable no-console */
   console.error(err.name + ' : ' + err.message);
 }
+
+// функции для создания массива
+
+const descriptions = [
+  'Не заинстаграмил - не поел',
+  'Кек',
+  'Не подарили - а накодил',
+  'Свободу политзаключенным!',
+  'Мой апарт-отель в Геленжике',
+]
+
+const names = [
+  'Valera',
+  'Vasya',
+  'Alex',
+  'Alfred',
+]
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const getRandomArrayElement = (elements) => {
+  return elements[_.random(0, elements.length - 1)];
+};
+
+const createComments = () => {
+  const randomIndexComment = getRandomIntIncl(1, 30);
+
+  return {
+    id: randomIndexComment,
+    avatar: 'img/avatar-' + getRandomIntIncl(1, 6) +'.svg',
+    message: getRandomArrayElement(messages),
+    name: getRandomArrayElement(names),
+  };
+};
+
+const createPhotoDescription = (index) => {
+  index += 1;
+  const randomLikes = getRandomIntIncl(15, 200);
+
+  return {
+    id: index,
+    url: 'photos/' + index + '.jpg',
+    description: getRandomArrayElement(descriptions),
+    likes: randomLikes,
+    comments: createComments(),
+  };
+};
+
+const descriptionArray = new Array(25).fill(null).map((element, index) => createPhotoDescription(index));
+
+console.log(descriptionArray);
